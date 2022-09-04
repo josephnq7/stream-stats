4a2e497246693188602c27305f6a4bb193c7f63d
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2022-09-04 15:04:29 -0700

    added bootstrap css and pagination for Vuetable 2 plugin, updated UI for count_stream_by_game

--- resources/js/pages/stats/count_stream_by_game.vue
+++ resources/js/pages/stats/count_stream_by_game.vue
@@ -6,8 +6,18 @@
                           :api-mode="false"
                           :fields="fields"
                           :per-page="perPage"
-                          :data=this.data
-                ></vuetable>
+                          :css="css.table"
+                          :show-sort-icons="true"
+                          :data-manager="dataManager"
+                          pagination-path="pagination"
+                          @vuetable:pagination-data="onPaginationData"/>
+
+                <div style="padding-top:10px">
+                    <vuetable-pagination
+                        ref="pagination"
+                        :css="css.pagination"
+                        @vuetable-pagination:change-page="onChangePage" />
+                </div>
             </card>
         </div>
     </div>
@@ -15,15 +25,23 @@
 
 <script>
 import Vuetable from 'vuetable-2/dist/vuetable-2'
+// import VueTablePaginationBootstrap4 from "../../components/VueTablePaginationBootstrap4";
+import VuetablePagination from "vuetable-2/src/components/VuetablePagination";
 import axios from "axios";
+import _ from "lodash";
+import CssConfig from "../../plugins/bootstrap4CssConfig.js";
+
 
 export default {
     name: "count_stream_by_game",
     components: {
-        Vuetable
+        Vuetable,
+        VuetablePagination
+        // VueTablePaginationBootstrap4
     },
     data() {
         return {
+            css: CssConfig,
             fields: [
                 {
                     name: 'name',
@@ -40,15 +58,52 @@ export default {
     },
     watch: {
         data(newVal, oldVal) {
-            debugger;
+            // debugger;
             this.$refs.vuetable.refresh();
         }
     },
     mounted() {
-        debugger;
+        // debugger;
         axios.get("/api/stats/total-streams-by-game").then(response => {
             this.data = response.data.data;
         });
     },
+
+    methods: {
+        onPaginationData(paginationData) {
+            this.$refs.pagination.setPaginationData(paginationData);
+        },
+        onChangePage(page) {
+            this.$refs.vuetable.changePage(page);
+        },
+        dataManager(sortOrder, pagination) {
+            if (this.data.length < 1) return;
+
+            let local = this.data;
+
+            // sortOrder can be empty, so we have to check for that as well
+            if (sortOrder.length > 0) {
+                console.log("orderBy:", sortOrder[0].sortField, sortOrder[0].direction);
+                local = _.orderBy(
+                    local,
+                    sortOrder[0].sortField,
+                    sortOrder[0].direction
+                );
+            }
+
+            pagination = this.$refs.vuetable.makePagination(
+                local.length,
+                this.perPage
+            );
+            console.log('pagination:', pagination)
+            let from = pagination.from - 1;
+            let to = from + this.perPage;
+
+            return {
+                pagination: pagination,
+                data: _.slice(local, from, to)
+            };
+        }
+    }
 }
 </script>
